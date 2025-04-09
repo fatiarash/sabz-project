ee12277c66efbd1e7d63a0edb23b57456e8b524f
elnaz <fatemeharash7@gmail.com>
2025-04-09 14:28:31 +0330

    comment

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/sabz-project/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  // base: "/sabz-project/",
+  // build: {
+  //   outDir: "dist",
+  //   emptyOutDir: true,
+  // },
 });
